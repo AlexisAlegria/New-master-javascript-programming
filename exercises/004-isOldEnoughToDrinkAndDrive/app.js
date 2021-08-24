5e167edaacc89457eb576d32093e3569e1cd886e
AlexisAlegria <alexisalegriav@gmail.com>
2021-08-24 16:09:19 +0000

    compiled up to exercise 06

--- exercises/004-isOldEnoughToDrinkAndDrive/app.js
+++ exercises/004-isOldEnoughToDrinkAndDrive/app.js
@@ -1,3 +1,7 @@
 function isOldEnoughToDrinkAndDrive(age) {
   // your code here
+  if(age>=21) return true;
+  else return false;
 }
+var output = isOldEnoughToDrinkAndDrive(22);
+console.log(output);
