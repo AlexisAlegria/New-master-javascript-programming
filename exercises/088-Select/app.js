e14ab8608cac87e0223ebddac1c11148ff1b6a7b
AlexisAlegria <alexisalegriav@gmail.com>
2021-08-27 01:45:43 +0000

    up to exercise 88

--- exercises/088-Select/app.js
+++ exercises/088-Select/app.js
@@ -1,1 +1,21 @@
 // Write your function here
+function select(arr, obj){
+    let newObj = {};
+    for(key in obj){
+        for(i=0; i<arr.length; i++){
+            if(arr[i] === key){
+                newObj[key] = obj[key];
+            }
+        }
+    }
+    return newObj;
+}
+var arr = ['a', 'c', 'e'];
+var obj = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4
+};
+var output = select(arr, obj);
+console.log(output); // --> { a: 1, c: 3 }
